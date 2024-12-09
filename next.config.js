50aee0b2e529833446ff4460aaa67693b9a7ea8d
unknown <ignaciomazuran12@gmail.com>
2024-12-09 11:54:33 -0300

    imgur

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
   reactStrictMode: true,
   output: 'export',
-  basePath: '/sofi', // Cambia esto al nombre de tu repositorio en GitHub
-  assetPrefix: '/sofi',
+  basePath: isProd ? '/sofi' : '', // Cambia esto al nombre de tu repositorio en GitHub
+  assetPrefix: isProd ? '/sofi' : '',
   // Comentamos la línea para desactivar el bundle analyzer por ahora
   // ...withBundleAnalyzer({
 };
